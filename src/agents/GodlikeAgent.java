b6145b4f6820f5f481ac48906cbb0e16a502b92a
Uncrowned <ivashchenko.leo@gmail.com>
2016-01-27 14:44:53 +0600

    add empty god like agent

--- src/agents/GodlikeAgent.java
+++ src/agents/GodlikeAgent.java
@@ -1,7 +1,17 @@
 package agents;
 
-/**
- * Created by sevenbits on 27.01.16.
- */
-public class GodlikeAgent {
+import jade.core.Agent;
+
+public class GodlikeAgent extends Agent {
+    public GodlikeAgent() {
+    }
+
+    protected void setup() {
+        //create map with messages to handle with, for regions
+        //we have to load files from some directory, and for each file
+        //1) create params map
+        //2) create "peoples" map
+        //3) create region, pass all arguments to it
+        //invoke migration for all "peoples"
+    }
 }
